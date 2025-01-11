14f772b70e3670d13cff7332801a675fde9ad102
hjpark625 <zero950@gmail.com>
2025-01-11 15:35:13 +0900

    feat: 초기 라우트 구성

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,8 @@ import { RenderMode } from '@angular/ssr'
 import type { ServerRoute } from '@angular/ssr'
 
 export const serverRoutes: ServerRoute[] = [
+  { path: '', renderMode: RenderMode.Client },
+  { path: 'home', renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import type { Routes } from '@angular/router'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('@app/index/index.component').then((C) => C.IndexComponent) },
+  { path: 'home', loadComponent: () => import('@app/home/home.component').then((C) => C.HomeComponent) }
+]
