52b19cd154856e4e259e2eea22e9765a3e147b74
hjpark625 <zero950@gmail.com>
2025-01-11 14:59:15 +0900

    initial setting

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,14 +1,13 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRoutesConfig } from '@angular/ssr';
-import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
+import { mergeApplicationConfig } from '@angular/core'
+import { provideServerRendering } from '@angular/platform-server'
+import { provideServerRoutesConfig } from '@angular/ssr'
+import { appConfig } from './app.config'
+import { serverRoutes } from './app.routes.server'
+
+import type { ApplicationConfig } from '@angular/core'
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes)
-  ]
-};
+  providers: [provideServerRendering(), provideServerRoutesConfig(serverRoutes)]
+}
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const config = mergeApplicationConfig(appConfig, serverConfig)
